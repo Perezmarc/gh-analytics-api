b26cbc599b851007209385d0f4df6ab308324c19
Marc Pérez <mpm792@gmail.com>
2017-09-11 17:25:42 +0200

    ADD: search service (still no internet, pushing from cafe)

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+// ******** IMPORTANT *******: Delete these lines on production
+// CLIENT_ID = 725f691a078864d5d24e
+// CLIENT_SECRET = b64b21ac0985db70f2e2415d8d4a7db6996c014c
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const GH_API_URL = 'https://api.github.com';
+const AUTH_PARAMS = `client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`;
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+app.use(allowCrossDomain);
+
+app.get('/', (req, res) => {
+  res.send(`Hello from Express!`)
+})
+
+app.get('/search/repos/', (req, res) => {
+  res.set('Content-Type', 'application/json');
+  console.log('trying to search');
+  const url = `${GH_API_URL}/search/repos?q=${req.query.q}&sort=stars&order=desc&${AUTH_PARAMS}`;
+
+  axios.get(url).then(response => {
+    res.status(200).json(response.body);
+  }).catch((error) => {
+      res.send(error);
+  });
+
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
